3960e337a1e929ce4e6553e0954d33f1be594f60
gabriel christino <gabriel.christino@gmail.com>
2023-08-01 11:00:55 -0300

    fix: arduino leonardo, mm para um

--- 3d_mini_resin/3d_mini_resin.ino
+++ 3d_mini_resin/3d_mini_resin.ino
@@ -1,18 +1,16 @@
-#define TEMPO_CURA  500 // tempo de cura da resina pela luz UV (ms)
+#define TEMPO_CURA  3000 // tempo de cura da resina pela luz UV (ms)
 #define TEMPO_PAUSA 500 // tempo de cura da resina pela luz UV (ms)
 
-#define PASSOS_MM  2038*3 // passos do motor por mm movimentado
-#define INTERVALO_PASSOS  5 // intervalo entre cada passo
+#define PASSOS_UM  2038*3/1000 // passos do motor por mm movimentado
+#define INTERVALO_PASSOS  1 // intervalo entre cada passo
+#define PASSOS_VOLTA  8 // passos / volta = 4 ou 8
 
 #include <Adafruit_GFX.h>         // Core graphics library
 #include <Adafruit_ILI9341.h>     // Hardware-specific library
 #include <SdFat.h>                // SD card & FAT filesystem library
 #include <Adafruit_SPIFlash.h>    // SPI / QSPI flash library
 #include <Adafruit_ImageReader.h> // Image-reading functions
-
-#include <Stepper.h>
  
-
 #define SD_CS  10 // SD card select pin
 #define TFT_CS  9 // TFT select pin
 #define TFT_DC  8 // TFT display/command pin
@@ -24,7 +22,16 @@ Adafruit_ILI9341     tft    = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 File root;
 
-const int steps[9][4] = {
+/*const bool steps[5][4] = {
+{1,1,0,0},
+{0,1,1,0},
+{0,0,1,1},
+{1,0,0,1},
+{0,0,0,0},
+};*/
+
+
+const bool steps[9][4] = {
 {1,0,0,0},
 {1,1,0,0},
 {0,1,0,0},
@@ -36,35 +43,38 @@ const int steps[9][4] = {
 {0,0,0,0},
 };
 
+
 void setup(void) {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   
   habilitaPortas();
   iniciaTFT();
-  //iniciaSD();
+  iniciaSD();
 }
 
 void habilitaPortas() {
-  pinMode(A1, INPUT);
+  pinMode(A3, INPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
+
+  //DDRD = B00111110;
   
-  digitalWrite(3, LOW);
+  /*digitalWrite(3, LOW);
   digitalWrite(4, LOW);
   digitalWrite(5, LOW);
   digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
+  digitalWrite(7, LOW);*/
 }
 
 void iniciaTFT() {
   tft.begin();
-  tft.fillScreen(ILI9341_BLACK);
   tft.setRotation(0);
-  delay(1000);
-  Serial.println("tela");
+  //tft.fillScreen(ILI9341_BLACK);
+  //delay(1000);
+  //Serial.println("tela");
 }
 
 void iniciaSD() {
@@ -74,23 +84,23 @@ void iniciaSD() {
   }
   
   tft.fillScreen(ILI9341_CYAN);
-  //Serial.println(SD.ls("/",LS_DATE));
+  ////Serial.println(SD.ls("/",LS_DATE));
 
   root = SD.open("/");
-  delay(1000);
+  //delay(1000);
 }
 
 bool impressaoFinalizada = false;
 int arquivo = 0;
 
-void printFiles(File dir, int numTabs)
+void printFiles(File dir)
 {
   while (!impressaoFinalizada) {
     File entry =  dir.openNextFile();
     if (! entry)
     {
       impressaoFinalizada = true;
-      moverMM(1);
+      moverUM(100);
       return;
     }
 
@@ -106,26 +116,26 @@ void printFiles(File dir, int numTabs)
     tft.fillScreen(ILI9341_BLACK);
     delay(TEMPO_PAUSA);
 
-    moverMM(0.2);
-    moverMM(-0.1);
+    moverUM(200);
+    moverUM(-100);
     arquivo++;
     delay(TEMPO_PAUSA);
     
   }
 }
 
-void moverMM(float mm) {
-  bool dir = mm > 0;
+void moverUM(int um) {
+  bool dir = um > 0;
   int passo = 0;
-  int passos = PASSOS_MM * abs(mm);
-  int voltas = passos / 8;
-  int sobraPassos = passos % 8;
+  int passos = PASSOS_UM * abs(um);
+  int voltas = passos / PASSOS_VOLTA;
+  byte sobraPassos = passos % PASSOS_VOLTA;
   while(voltas >= 0) {
-    while((voltas > 0 && passo < 8) || (voltas == 0 && passo < sobraPassos)) {
-      digitalWrite(4, steps[passo][dir ? 0 : 3]);
-      digitalWrite(5, steps[passo][dir ? 1 : 2]);
-      digitalWrite(6, steps[passo][dir ? 2 : 1]);
-      digitalWrite(7, steps[passo][dir ? 3 : 0]);
+    while((voltas > 0 && passo < PASSOS_VOLTA) || (voltas == 0 && passo < sobraPassos)) {
+      digitalWrite(4, steps[passo][!dir ? 0 : 3]);
+      digitalWrite(5, steps[passo][!dir ? 1 : 2]);
+      digitalWrite(6, steps[passo][!dir ? 2 : 1]);
+      digitalWrite(7, steps[passo][!dir ? 3 : 0]);
       passo++;
       delay(INTERVALO_PASSOS);
     }
@@ -133,24 +143,25 @@ void moverMM(float mm) {
     voltas--;
   }
   
-  digitalWrite(4, steps[8][0]);
-  digitalWrite(5, steps[8][1]);
-  digitalWrite(6, steps[8][2]);
-  digitalWrite(7, steps[8][3]);
+  digitalWrite(4, steps[PASSOS_VOLTA][0]);
+  digitalWrite(5, steps[PASSOS_VOLTA][1]);
+  digitalWrite(6, steps[PASSOS_VOLTA][2]);
+  digitalWrite(7, steps[PASSOS_VOLTA][3]);
 }
+
 
 bool teclaPressionada = false;
 
 void loop() {
   if (!teclaPressionada) {
-    Serial.println("mover");
-    moverMM(0.1);
-    teclaPressionada = digitalRead(A1);
+    //Serial.println("mover");
+    moverUM(-100);
+    teclaPressionada = digitalRead(A3);
   }
   if (teclaPressionada && !impressaoFinalizada) {
-    moverMM(0.2);
+    moverUM(200);
     tft.fillScreen(ILI9341_BLACK);
     delay(1000);
-    printFiles(root,0);
+    printFiles(root);
   }
 }
